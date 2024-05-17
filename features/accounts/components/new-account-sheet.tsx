c0feb30a36e128f91bf4800275785d5cdd3310c4
Amrit <amritpandey2005@gmail.com>
2024-05-17 17:34:39 +0530

    finishing account post api

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -6,9 +6,27 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+import { useCreateAccount } from "../api/use-create-accounts";
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const {isOpen, onClose} = useNewAccount()
+
+  const mutation = useCreateAccount()
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values)
+  }
+
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
@@ -18,6 +36,7 @@ export const NewAccountSheet = () => {
             Create a new account to track your applications.
           </SheetDescription>
         </SheetHeader>
+      <AccountForm onSubmit={onSubmit} disabled={false} defaultValues={{name: ''}}/>
       </SheetContent>
     </Sheet>
   );
